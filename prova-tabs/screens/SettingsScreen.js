7e1661089eb61f926e958419795756d46f1e5458
BernatIV <marc.mane.duatis@gmail.com>
2019-11-01 16:28:57 +0100

    Fix QR image

--- prova-tabs/screens/SettingsScreen.js
+++ prova-tabs/screens/SettingsScreen.js
@@ -11,7 +11,7 @@ export default function SettingsScreen() {
   return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Image style={styles.avatar} source={{uri: 'https://cdn.qrstuff.com/images/default_qrcode.png'}}/>
+          <Image style={styles.header} source={{uri: 'https://cdn.qrstuff.com/images/default_qrcode.png'}}/>
         </View>
         <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
         <View style={styles.body}>
